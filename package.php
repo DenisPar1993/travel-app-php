2828cd57a2299049be9d46fbd765208e278b91c4
DenisPar1993 <whatsmyageagain@mail.ru>
2022-12-20 23:28:34 +0300

    home and slider

---
+++ package.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+
+
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"
+/>
+<link  rel="stylesheet" href="css/style.css"/>
+    
+</head>
+<body>
+  <section class="header">
+    <a class="logo" href="home.php">travel</a>
+    <nav class="navbar">
+        <a href="home.php">home</a>
+        <a href="about.php">about</a>
+        <a href="book.php">book</a>
+        <a href="package.php">package</a>
+    </nav>
+
+    <div id="menu-btn" class="fas fa-bars"></div>
+  </section>
+ 
+<div class="heading" style = "background:url(images/header-bg-2.png) no-repeat">
+ <h1>Package</h1>
+</div>
+
+
+<section class="footer">
+    <div class="box-container">
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="home.php"><i class="fas fa-angle-right"></i>home</a>
+        <a href="about.php"><i class="fas fa-angle-right"></i>about</a>
+        <a href="book.php"><i class="fas fa-angle-right"></i>book</a>
+        <a href="package.php"><i class="fas fa-angle-right"></i>package</a>
+        </div>
+        <div class="box">
+            <h3>extra link</h3>
+            <a href="#"><i class="fas fa-angle-right"></i>ask question</a>
+            <a href="#"><i class="fas fa-angle-right"></i>about us</a>
+            <a href="#"><i class="fas fa-angle-right"></i>privacy policy</a>
+            <a href="#"><i class="fas fa-angle-right"></i>terms of use</a>
+
+        </div>
+        <div class="box">
+            <h3>contact info</h3>
+            <a href="#"><i class="fas fa-phone"></i>888-888-8888</a>
+            <a href="#"><i class="fas fa-phone"></i>888-888-8888</a>
+            <a href="#"><i class="fas fa-envelope"></i>usr@gmail.com</a>
+            <a href="#"><i class="fas fa-map"></i>Paris,France</a>
+          
+
+        </div>
+        <div class="box">
+          <h3>follow us</h3>
+          <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+          <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+          <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+          <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+        </div>
+    </div>
+</section>
+
+
+<script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
+ <script src="js/script.js"></script>
+</body>
+</html>
